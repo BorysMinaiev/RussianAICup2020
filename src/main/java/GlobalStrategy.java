7d0a24bb71d5627e74a6d8c485d90f21a8e5291b
Borys Minaiev <bminaiev@gmail.com>
2020-12-06 11:35:47 +0000

    refactoring

--- src/main/java/GlobalStrategy.java
+++ src/main/java/GlobalStrategy.java
@@ -1,5 +1,8 @@
 import model.EntityType;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static model.EntityType.*;
 
 public class GlobalStrategy {
@@ -14,20 +17,48 @@ public class GlobalStrategy {
         return state.populationTotal - state.populationUsed < 10;
     }
 
+    private boolean needRangedHouse() {
+        return state.myEntitiesCount.get(RANGED_BASE) == 0;
+    }
+
+    static class ExpectedEntitiesDistribution {
+        Map<EntityType, Integer> count;
+
+        ExpectedEntitiesDistribution(int builderNum, int turretsNum, int rangesNum) {
+            count = new HashMap<>();
+
+            count.put(BUILDER_UNIT, builderNum);
+            count.put(TURRET, turretsNum);
+            count.put(RANGED_UNIT, rangesNum);
+        }
+
+        EntityType chooseWhatToBuild(final State state) {
+            // 0 -> not enough
+            // 787788 -> too much
+            double smallestCoef = Double.MAX_VALUE;
+            EntityType best = null;
+            for (Map.Entry<EntityType, Integer> entry : count.entrySet()) {
+                int currentCnt = state.myEntitiesCount.get(entry.getKey());
+                double curCoef = currentCnt / (double) entry.getValue();
+                if (curCoef < smallestCoef) {
+                    smallestCoef = curCoef;
+                    best = entry.getKey();
+                }
+            }
+            return best;
+        }
+
+        static ExpectedEntitiesDistribution V1 = new ExpectedEntitiesDistribution(5, 1, 1);
+    }
+
     EntityType whatNextToBuild() {
+        if (needRangedHouse()) {
+            return RANGED_BASE;
+        }
         if (needMoreHouses()) {
             return HOUSE;
         }
+        return ExpectedEntitiesDistribution.V1.chooseWhatToBuild(state);
         // TODO: make it smarter
-        int buildersNum = state.myEntitiesCount.get(BUILDER_UNIT);
-        int turretsNum = state.myEntitiesCount.get(TURRET);
-        int rangesNum = state.myEntitiesCount.get(RANGED_UNIT);
-        if (rangesNum * 5 < buildersNum) {
-            return RANGED_UNIT;
-        }
-        if (turretsNum * 5 < buildersNum) {
-            return TURRET;
-        }
-        return BUILDER_UNIT;
     }
 }
